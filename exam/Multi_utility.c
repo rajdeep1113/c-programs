83f36e52a625ecd6cad29cabf597561cb9287a8a
HappyOP411 <rajdeepsinh7568@gmail.com>
2024-05-31 17:41:41 +0530

    second

---
+++ exam/Multi_utility.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int main(){
+
+//Ans 1
+
+// float c=38;
+// float f;
+// f=(c*9/5)+32;
+// printf("the temprature is %.2f",f);
+
+// Ans 2
+
+//    int basesalary,hra,da,ta;
+//     printf("Enter your base salary :");
+//     scanf("%d",&basesalary);
+//     printf("Enter HRA :");
+//     scanf("%d",&hra);
+//     printf("Enter da :");
+//     scanf("%d",&da);
+//     printf("enter ta :");
+//     scanf("%d",&ta);
+//     printf("your basesalary is %d, your hra is %d, your da is %d and your ta ia %d\n",basesalary,hra,da,ta);
+//     int grossSalary=basesalary+(basesalary*hra/100)+(basesalary*da/100)+(basesalary*ta/100);
+//     printf("grossSalary is %d\n",grossSalary);
+
+
+
+// Ans 3
+
+// float a1,a2,a3;
+// printf("Enter your a1:");
+// scanf("%f",&a1);
+// printf("Enter your a1:");
+// scanf("%f",&a2);
+// a3=180.0-(a1+a2);
+// printf("your a3 is: %.2f",a3);
+
+
+
+    return 0;
+}
